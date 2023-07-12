5061c0009d5c6ccd5201f8b499b6e705fd10cb8d
mmckenzieORCEN <137900372+mmckenzieORCEN@users.noreply.github.com>
2023-07-12 09:45:29 -0400

    Update setup.py

--- root/setup.py
+++ root/setup.py
@@ -5,6 +5,7 @@ setup(
     version="1.0",
     description="A package for CLIP-based image and text processing.",
     packages=["clipcraft"],
+    download_url='https://github.com/mmckenzieORCEN/CLIPCraft/releases/download/Release/clipcraft-1.0.tar.gz'
     install_requires=[
         "transformers",
         "Pillow",
