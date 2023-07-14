c94eba291efd2fccb09687147e3d834b1bc29d23
mmckenzieORCEN <137900372+mmckenzieORCEN@users.noreply.github.com>
2023-07-14 10:33:56 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 
+
+with open('README.md', 'r', encoding='utf-8') as readme_file:
+    long_description = readme_file.read()
+
 setup(
     name="clipcraft",
     version="1.1",
@@ -7,6 +11,9 @@ setup(
     author='Morgan McKenzie',
     author_email='morgancmckenziecs@gmail.com'
     packages=["clipcraft"],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=https://github.com/mmckenzieORCEN/CLIPCraft,
     install_requires=[
         "transformers",
         "Pillow",
